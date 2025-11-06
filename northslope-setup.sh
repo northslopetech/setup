dec1fb21f15ecc496562c3f52079cd7e4ef0e527
Tam Nguyen <tnguyen@northslopetech.com>
2025-11-05 22:07:48 -0500

    Refactor to clarify intention

--- northslope-setup.sh
+++ northslope-setup.sh
@@ -4,6 +4,12 @@ function get_latest_version {
     curl -sL https://api.github.com/repos/northslopetech/setup/releases/latest | jq -r '.tag_name' | sed 's/\s//g' | sed 's/\n//g'
 }
 
+function print_usage {
+    echo "Usage: setup"
+    echo "   This command will run the northslope setup script for your machine."
+    echo "   Your Version  : ${northslope_setup_version}"
+    echo "   Latest Version: $(get_latest_version)"
+}
 
 northslope_setup_version=`cat ${HOME}/.northslope-setup-version`
 
@@ -11,10 +17,7 @@ if [[ ! -z $1 ]]; then
     # Prints the help message if any
     # arguments are passed. In the future,
     # we can check to make sure it's a '--help'
-    echo "Usage: setup"
-    echo "   This command will run the northslope setup script for your machine."
-    echo "   Your Version  : ${northslope_setup_version}"
-    echo "   Latest Version: $(get_latest_version)"
+    print_usage
     exit 1
 fi
 
